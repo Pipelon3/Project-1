0c8f343d37b61e4fe6edfefde5e0aac3af3adeba
tyler <tmillner17@gmail.com>
2019-06-27 19:39:28 -0600

    javascript changes

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,47 +1,31 @@
-//inital aray of games
+//inital aray of info
+"use strict"
+let database = firebase.database();
+var queryURL = 
 
-var games = ["The Legend of Zelda", "Bioshock", "Kingdom Hearts", "The Witcher"];
+//Need to change API here to search info API
 
-//Need to change API here to game info API
+  var firebaseConfig = {
+    apiKey: "AIzaSyDdNeSGejyUNnUfuVViu5gTiGJ5Ur3Coco",
+    authDomain: "our-group-project.firebaseapp.com",
+    databaseURL: "https://our-group-project.firebaseio.com",
+    projectId: "our-group-project",
+    storageBucket: "our-group-project.appspot.com",
+    messagingSenderId: "188419258236",
+    appId: "1:188419258236:web:16bd2d51ed10d949"
+  };
+  firebase.initializeApp(firebaseConfig);
 
-function displayGameInfo() {
-    var game = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=5KZDAjtUy2sVI1b6KIs3blyHtx4vUAK9&q=" + game + "&limit=10&offset=0&rating=R&lang=en";
 
     $.ajax({
       url: queryURL,
       contentType: "text/html",
       method: "GET"
-      }).then(function(response) {
-        $("#game-view").empty();
-        for ( var i = 0; i < response.data.length; i ++){
-          var a = $("<iframe>");
-          a.addClass("game");
-          a.attr('src', response.data[i].embed_url);
-          a.attr('type', response.data[i].type);
-          $("#game-view").append(a);
-        }         
-      });            
-    }
+    }).then(function(response) {  
+      console.log(response);    
+      });         
 
 function renderButtons() {
-    $("#buttons-view").empty();
-    for (var i = 0; i < games.length; i++) {
-        var a = $("<button>");
-        a.addClass("game-btn");
-        a.attr("data-name", games[i]);
-        a.text(games[i]);
-        $("#buttons-view").append(a);
+    
     }
 }
-
-$("#add-game").on("click", function(event) {
-    event.preventDefault();
-    var game = $("#game-input").val().trim();
-    games.push(game);
-    renderButtons();
-});
-
-$(document).on("click", ".game-btn", displayGameInfo);
-
-renderButtons();
