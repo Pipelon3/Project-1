889b0038aa029f60fb2e71eb3cf8be3aa4a9b708
Pipelon3 <Pipelon999@gmail.com>
2019-06-29 12:19:40 -0600

    JS fixes

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,14 +1,13 @@
+let myqueryURL = "http://www.omdbapi.com/?apikey=trilogy"
+let firebaseConfig = {
+  apiKey: "AIzaSyDdNeSGejyUNnUfuVViu5gTiGJ5Ur3Coco",
+  authDomain: "our-group-project.firebaseapp.com",
+  databaseURL: "https://our-group-project.firebaseio.com",
+  projectId: "our-group-project",
+  storageBucket: "our-group-project.appspot.com",
+  messagingSenderId: "188419258236",
+  appId: "1:188419258236:web:16bd2d51ed10d949"
+};
 
-
-//Need to change API here to search info API
-
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=5KZDAjtUy2sVI1b6KIs3blyHtx4vUAK9&q=" + search + "&limit=10&offset=0&rating=R&lang=en";
-
-    $.ajax({
-      url: queryURL,
-      contentType: "text/html",
-      method: "GET"
-      }).then(function(response) {
-        console.log(response);
-        }         
-      )
+firebase.initializeApp(firebaseConfig);
+let database = firebase.database();
