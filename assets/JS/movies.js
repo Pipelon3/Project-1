2f8d52a6dc0359db6dadc13be10c97e3746d6763
Pipelon3 <Pipelon999@gmail.com>
2019-07-05 17:21:14 -0600

    buttons work now

--- assets/JS/movies.js
+++ assets/JS/movies.js
@@ -1,81 +1,57 @@
 //Stuff to make Movie info appear
 
 $("#submitPress").on("click", function() {
-let movie = $("#user-input").val().trim();
-let queryURL = myqueryURL + "&query="  + movie 
-$.ajax({
-url: queryURL,
-method: "GET"
-}).then(function (response) {
-let results = response;
-console.log(results)
-$("#info-appear-here").empty();
-let title = $("<h2>").text("Title: " + results.results[0].title);
-$("#info-appear-here").append(title)
-let movieDiv = $("<img>"); 
-$("#movie-view").prepend(movieDiv)
-movieDiv.attr("src", "https://image.tmdb.org/t/p/w500" + results.results[0].poster_path)
-$("#movie-view").append(movieDiv)
-let plot = $("<p>").text("Plot: " + results.results[0].overview);
-$("#info-appear-here").append(plot)
-let release = $("<p>").text("Release Date: " + results.results[0].release_date)
-$("#info-appear-here").append(release)
-//let rating = $("<p>").text("Rating: "  + results.results[0])
-//$("#info-appear-here").append(rating)
-
-})
-event.preventDefault();
-let movieName = $("#user-input").val().trim();
-let newMovie = {
-name: movieName,
-}
-database.ref().push(newMovie)
-console.log(newMovie.name)
+    let movie = $("#user-input").val().trim();
+    displayMovieInfo(movie)
+    event.preventDefault();
+    movies.push(movie);
+    renderButtons();
 });
+
+$(document).on("click", ".movie-btn", showMovieInfo)
 
 //Stuff to make the buttons show up
 
-// var movies = [];
+var movies = [];
 
-// function displaymovieInfo() {
-//     var movie = $(this).attr("data-name");
-//     var queryURL = "https://api.themoviedb.org/4/search/movie?api_key=a22239c0a3a0a19a69effb094f260e6a + movie;
+function showMovieInfo() {
+    var movie = $(this).attr("data-name");
+    displayMovieInfo(movie);           
+}
 
-//     $.ajax({
-//       url: queryURL,
-//       contentType: "text/html",
-//       method: "GET"
-//       }).then(function(response) {
-//         $("#movie-view").empty();
-//         for ( var i = 0; i < response.data.length; i ++){
-//           var a = $("<iframe>");
-//           a.addClass("movie");
-//           a.attr('src', response.data[i].embed_url);
-//           a.attr('type', response.data[i].type);
-//           $("#movie-view").append(a);
-//         }         
-//       });            
-// }
+function renderButtons() {
+    $("#buttons-view").empty();
+    for (var i = 0; i < movies.length; i++) {
+        var a = $("<button>");
+        a.addClass("movie-btn");
+        a.attr("data-name", movies[i]);
+        a.text(movies[i]);
+        $("#buttons-view").append(a);
+    }
+}
 
-// function renderButtons() {
-//     $("#buttons-view").empty();
-//     for (var i = 0; i < movies.length; i++) {
-//         var a = $("<button>");
-//         a.addClass("movie-btn");
-//         a.attr("data-name", movies[i]);
-//         a.text(movies[i]);
-//         $("#buttons-view").append(a);
-//     }
-// }
+function displayMovieInfo(movie) {
+    let queryURL = myqueryURL + "&query="  + movie 
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+        }).then(function (response) {
+        let results = response;
+        console.log(results)
+        $("#info-appear-here").empty();
+        let title = $("<h2>").text("Title: " + results.results[0].title);
+        $("#info-appear-here").append(title)
+        let movieDiv = $("<img>"); 
+        $("#movie-view").prepend(movieDiv)
+        movieDiv.attr("src", "https://image.tmdb.org/t/p/w500" + results.results[0].poster_path)
+        $("#movie-view").append(movieDiv)
+        let plot = $("<p>").text("Plot: " + results.results[0].overview);
+        $("#info-appear-here").append(plot)
+        let release = $("<p>").text("Release Date: " + results.results[0].release_date)
+        $("#info-appear-here").append(release)
+        
+        })
+}
 
-// $("#add-movie").on("click", function(event) {
-//     event.preventDefault();
-//     var movie = $("#movie-input").val().trim();
-//     movies.push(movie);
-//     renderButtons();
-// });
-
-// $(document).on("click", ".movie-btn", displaymovieInfo);
-
-// renderButtons();
+renderButtons();
 
--- assets/JS/shows.js
+++ assets/JS/shows.js
@@ -1,33 +1,56 @@
-//Stuff to make Show info appear
+//Stuff to make show info appear
 
 $("#submitPress").on("click", function() {
-let show = $("#user-input").val().trim();
-let queryURL = TVshowURL + "&query="  + show
-$.ajax({
- url: queryURL,
- method: "GET"
-}).then(function (response) {
- let results = response;
- console.log(results)
- $("#info-appear-here").empty();
- let title = $("<h2>").text("Title: " + results.results[0].name);
- $("#info-appear-here").append(title)
- let showDiv = $("<img>"); 
- $("#show-view").prepend(showDiv)
- showDiv.attr("src", "https://image.tmdb.org/t/p/w500" + results.results[0].poster_path)
- $("#show-view").append(showDiv)
- let plot = $("<p>").text("Plot: " + results.results[0].overview);
- $("#info-appear-here").append(plot)
- let rating = $("<p>").text("First Aired: "  + results.results[0].first_air_date)
- $("#info-appear-here").append(rating)
-
-})
-event.preventDefault();
-let showName = $("#user-input").val().trim();
-let newShow = {
- name: showName,
-}
-database.ref().push(newShow)
-console.log(newShow.name)
-
+    let show = $("#user-input").val().trim();
+    displayshowInfo(show)
+    event.preventDefault();
+    shows.push(show);
+    renderButtons();
 });
+
+$(document).on("click", ".show-btn", showshowInfo)
+
+//Stuff to make the buttons show up
+
+var shows = [];
+
+function showshowInfo() {
+    var show = $(this).attr("data-name");
+    displayshowInfo(show);           
+}
+
+function renderButtons() {
+    $("#buttons-view").empty();
+    for (var i = 0; i < shows.length; i++) {
+        var a = $("<button>");
+        a.addClass("show-btn");
+        a.attr("data-name", shows[i]);
+        a.text(shows[i]);
+        $("#buttons-view").append(a);
+    }
+}
+
+function displayshowInfo(show) {
+    let queryURL = TVshowURL + "&query="  + show
+    $.ajax({
+     url: queryURL,
+     method: "GET"
+    }).then(function (response) {
+     let results = response;
+     console.log(results)
+     $("#info-appear-here").empty();
+     let title = $("<h2>").text("Title: " + results.results[0].name);
+     $("#info-appear-here").append(title)
+     let showDiv = $("<img>"); 
+     $("#show-view").prepend(showDiv)
+     showDiv.attr("src", "https://image.tmdb.org/t/p/w500" + results.results[0].poster_path)
+     $("#show-view").append(showDiv)
+     let plot = $("<p>").text("Plot: " + results.results[0].overview);
+     $("#info-appear-here").append(plot)
+     let rating = $("<p>").text("First Aired: "  + results.results[0].first_air_date)
+     $("#info-appear-here").append(rating)
+    
+        })
+}
+
+renderButtons();
