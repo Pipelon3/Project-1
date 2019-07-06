8bfccda481432ba9925b7a3b296cf2b4678cffd4
Brandon Lang <brandonlang1997@gmail.com>
2019-07-06 07:39:31 -0600

    attempts to fix js dont add yet

--- assets/JS/shows.js
+++ assets/JS/shows.js
@@ -1,28 +1,4 @@
-//Stuff to make Show info appear
 
-$("#submitPress").on("click", function() {
-let show = $("#user-input").val().trim();
-let queryURL = TVshowURL + "&query="  + show
-$.ajax({
- url: queryURL,
- method: "GET"
-}).then(function (response) {
- let results = response;
- console.log(results)
- $("#info-appear-here").empty();
- let title = $("<h2>").text("Title: " + results.results[0].name);
- $("#info-appear-here").append(title)
- let showDiv = $("<img>"); 
- $("#show-view").prepend(showDiv)
- showDiv.attr("src", "https://image.tmdb.org/t/p/w500" + results.results[0].poster_path)
- $("#show-view").append(showDiv)
- let plot = $("<p>").text("Plot: " + results.results[0].overview);
- $("#info-appear-here").append(plot)
- let rating = $("<p>").text("First Aired: "  + results.results[0].first_air_date)
- $("#info-appear-here").append(rating)
-
-})
-});
 
 //Stuff to make show info appear
 
@@ -47,16 +23,16 @@ function showshowInfo() {
     displayshowInfo(show);           
 }
 
-function renderButtons() {
-    $("#buttons-view").empty();
-    for (var i = 0; i < shows.length; i++) {
-        var a = $("<button>");
-        a.addClass("show-btn");
-        a.attr("data-name", shows[i]);
-        a.text(shows[i]);
-        $("#buttons-view").append(a);
-    }
-}
+// function renderButtons() {
+//     $("#buttons-view").empty();
+//     for (var i = 0; i < shows.length; i++) {
+//         var a = $("<button>");
+//         a.addClass("show-btn");
+//         a.attr("data-name", shows[i]);
+//         a.text(shows[i]);
+//         $("#buttons-view").append(a);
+//     }
+// }
 
 function displayshowInfo(show) {
     let queryURL = TVshowURL + "&query="  + show
@@ -81,4 +57,4 @@ function displayshowInfo(show) {
         })
 }
 
-renderButtons();
+// renderButtons();
